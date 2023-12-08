db01bf9dc265820138f9601e32490f1ff864ce3b
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-08 18:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 function incrementFun() {
 	let counter=document.getElementById("counter").innerHTML;
 let btn=document.getElementById("incrementBtn");
+	            alert("Un-incremented value: " + counter);
+
 
 	let newCounter=parseInt(counter)+1;
 	counter.innerHTML=newCounter;
+	
 	
 	
 }
