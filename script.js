8f8f0f4d8003457e98e75ba49cbd202f458f9c50
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-11 14:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 //your JS code here. If required.
 
 function incrementFun() {
-	let counterElement=document.getElementById("counter");
-	let counterValue=counterElement.textContent;
-	
-	            alert("Un-incremented value: " + counterValue);
+	const counterElement = document.getElementById('counter');
+    const incrementBtn = document.getElementById('incrementBtn');
 
+    // Initial counter value
+    let counterValue = 0;
 
-	let newCounter=parseInt(counterValue)+1;
-	counterValue.textContent=newCounter;
+    // Function to handle button click
+    function handleIncrementClick() {
+      // Display the un-incremented value in an alert
+      alert(`Un-incremented value: ${counterValue}`);
+
+      // Increment the counter value
+      counterValue++;
+
+      // Update the counter element
+      counterElement.textContent = counterValue;
+    }
+
+    // Attach the click event listener to the button
+    incrementBtn.addEventListener('click', handleIncrementClick);
 	
 	
 	
