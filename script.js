b6922d2afedcac23ff22dc2cc30f25ff9d0bf9b6
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-11 14:39:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
 
 function incrementFun() {
-	let counter=document.getElementById("counter");
-	let counterValue=counter.textContent;
+	let counterElement=document.getElementById("counter");
+	let counterValue=counterElement.textContent;
 	
 	            alert("Un-incremented value: " + counterValue);
 
 
 	let newCounter=parseInt(counterValue)+1;
-	counterValue.value=newCounter;
+	counterValue.textContent=newCounter;
 	
 	
 	
