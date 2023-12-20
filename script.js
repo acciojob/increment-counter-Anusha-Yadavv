7b43ef86749e520ec9225c5e60256f7653ba31f2
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-20 18:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let pElement=document.getElementById("counter");
 let buttonElement=document.getElementById("incrementBtn");
 function incrementFun() {
 	buttonElement.style.backgroundColor="blue";
-	counter++;
-	pElement.innerText=counter.toString();
-	alert(`${counter}`)
+	// counter++;
+	counter+=1
+	pElement.innerText=counter+1;
+	alert(`${counter.toString()}`)
 	
 }
 
